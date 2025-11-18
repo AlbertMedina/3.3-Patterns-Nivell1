59bb30cf636e9e256964f82ce15137d677fb1f7f
Albert Medina <albertmecu@gmail.com>
2025-11-18 11:12:37 +0100

    feat: added mysql dependency in pom file + implemented data base connection

---
+++ src/main/java/db/DBConnection.java
@@ -0,0 +1,43 @@
+package db;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConnection {
+
+    private static final String PROPERTIES_FILE_PATH = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "db.properties";
+
+    private static String dbUrl;
+    private static String dbUser;
+    private static String dbPassword;
+
+    public DBConnection(){
+        try {
+            Properties properties = loadProperties(PROPERTIES_FILE_PATH);
+            String dbName = properties.getProperty("name");
+            String dbPort = properties.getProperty("port");
+            dbUrl = "jdbc:mysql://localhost:" + dbPort + "/" + dbName + "?useSSL=false&serverTimezone=UTC";
+            dbUser = properties.getProperty("user");
+            dbPassword = properties.getProperty("password");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private Properties loadProperties(String path) throws IOException {
+        Properties properties = new Properties();
+        try (FileInputStream inputStream = new FileInputStream(path)) {
+            properties.load(inputStream);
+        }
+        return properties;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+    }
+}
